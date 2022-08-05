9132889612a7fd26debdbd1cefb3cc7e1b5b5366
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-05 21:42:43 +0100

    Update urls_postgres.py

--- urls_postgres.py
+++ urls_postgres.py
@@ -1,2 +1,25 @@
 from utils import *
 import psycopg2
+
+
+def urls_from_sitemap():
+    for row, index in sitelist.iterrows():
+        subsitemappath = "collected/{}/subsitemap/".format(index["sitename"])
+        if "wikihow" not in subsitemappath:
+            files = (glob.glob("{}/{}".format(subsitemappath,"*.xml")))
+            for f in files:
+                try:
+                    f = open(f, 'r', encoding='UTF-8')
+                    res = f.readlines()
+                    for d in res:
+                        data = getUrl(str(d))
+                        for i in data:
+                            cleanurl = excludepatter(i)
+                            if (cleanurl) is not None:
+                                query = '''insert into articles (urls,sitename,lang) VALUES (%s,%s,%s) ON CONFLICT DO NOTHING'''
+                                pgcursor.execute(query, [str(cleanurl),  str(index["sitename"]), str(index["lang"])])
+                                print ("Submitted :: " , cleanurl)
+
+                except Exception as e :
+                    print(e)
+                    pass
