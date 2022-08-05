2c4d8f669a16fb2948123dc401a72d1ba4329777
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-05 23:11:32 +0100

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -43,6 +43,6 @@ def save_sites_sqlite():
         updatesql = '''update sitelist  set sitemapurl = ? where urls like ?; '''
         sqlitecursor.execute(updatesql,(index["sitemapurl"],str(index["urls"])))
         sqliteconn.commit()
-        
-        
+save_sites_sqlite()  
+create_sqlite_table()
 sitelistpd = pd.read_sql_query("SELECT * from sitelist", sqliteconn)
