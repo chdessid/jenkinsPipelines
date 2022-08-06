74022766153a0b81502144b3904d77b321817dc1
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-06 23:04:12 +0100

    Update move_urls_dev_stg.py

--- move_urls_dev_stg.py
+++ move_urls_dev_stg.py
@@ -19,9 +19,9 @@ for row, index in dev_records.iterrows():
     send_to_stg = ''' insert into articles (urls) values (%s) on conflict do nothing '''
     query_stg_pgcursor = stg_pgcursor.execute(send_to_stg,[index["urls"]])
     
-    addattributes = ''' update articles set name =%s,description =%s,devimages =%s,category =%s where urls =%s '''
+    addattributes = ''' update articles set name =%s,description =%s,devimages =%s where urls =%s '''
     query_stg_pgcursor_attributes = stg_pgcursor.execute(addattributes,[index["name"],index["description"],
-                                                                        index["category"],index["urls"]])
+                                                                        index["devimages"],index["urls"]])
     mark_stg_ok = ''' update articles set stg_ok=TRUE where urls =%s '''
     query_mark_stg_ok = dev_pgcursor.execute(mark_stg_ok,[index["urls"]])
     print ("STG RECORDS MOVED : {}".format(index["urls"]))
