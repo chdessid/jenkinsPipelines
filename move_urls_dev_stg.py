99208f628c840b7af934fa8f249382f087d3b8ad
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-06 22:18:34 +0100

    Create move_urls_dev_stg.py

---
+++ move_urls_dev_stg.py
@@ -0,0 +1,27 @@
+import psycopg2
+import pandas as pd
+import pandas.io.sql as sqlio
+
+
+dev_pgconn = psycopg2.connect(user="sqladmin",password="Cc.09275920",host="192.168.1.73",port="5432",database="jaridaa_dev")
+dev_pgcursor = dev_pgconn.cursor()
+dev_pgconn.autocommit = True
+
+stg_pgconn = psycopg2.connect(user="sqladmin",password="Cc.09275920",host="192.168.1.73",port="5432",database="jaridaa_stg")
+stg_pgcursor = stg_pgconn.cursor()
+stg_pgconn.autocommit = True
+
+selectall = ''' select * from articles where name is not null limit 1000 '''
+dev_records = sqlio.read_sql_query(selectall, dev_pgconn)
+
+for row, index in dev_records.iterrows():
+    print (index["urls"])
+
+    send_to_stg = ''' insert into articles (urls) values (%s) on conflict do nothing '''
+    query_stg_pgcursor = stg_pgcursor.execute(send_to_stg,[index["urls"]])
+    
+    print ("START MARKING AS STG_OK")
+    mark_stg_ok = ''' update articles set stg_ok=TRUE where urls =%s '''
+    query_mark_stg_ok = dev_pgcursor.execute(mark_stg_ok,[index["urls"]])
+    
+    print ("STG OK DONE ")
