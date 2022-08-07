8999ec5aea40ebffea8a9f29e2eae1adaf195d9c
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-07 21:47:10 +0100

    Update settings.py

--- scrapyextractors/scrapyextractors/settings.py
+++ scrapyextractors/scrapyextractors/settings.py
@@ -17,10 +17,14 @@ NEWSPIDER_MODULE = 'scrapyextractors.spiders'
 #USER_AGENT = 'scrapyextractors (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
