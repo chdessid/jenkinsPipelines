536d2ff9c556cda8713c29ba2da516187ec211a8
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-07 20:18:39 +0100

    Create sites_ar.py

---
+++ sites_ar.py
@@ -0,0 +1,29 @@
+from utils import *
+from sqlite import *
+
+sitelistpd = pd.read_sql_query("SELECT * from sitelist", sqliteconn)
+
+def urls_from_sitemap():
+    for row, index in sitelistpd.iterrows():
+        subsitemappath = "/var/lib/jenkins/workspace/DEV/collected/{}/subsitemap/".format(index["sitename"])
+        if "ar"  in index["lang"]:
+            files = (glob.glob("{}/{}".format(subsitemappath,"*.xml")))
+            for f in files:
+                try:
+                    f = open(f, 'r', encoding='UTF-8')
+                    res = f.readlines()
+                    for d in res:
+                        data = getUrl(str(d))
+                        for i in data:
+                            cleanurl = excludepatter(i)
+                            if (cleanurl) is not None:
+                                query = '''insert into articles (urls,sitename,lang) VALUES (%s,%s,%s) ON CONFLICT DO NOTHING'''
+                                pgcursor.execute(query, [str(cleanurl),  str(index["sitename"]), str(index["lang"])])
+                                print ("Submitted :: " , cleanurl)
+
+                except Exception as e :
+                    print(e)
+                    pass
+                
+                
+urls_from_sitemap()
