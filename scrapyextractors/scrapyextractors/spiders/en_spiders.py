4079d24c814a2a73bd34b7a2e5d0df73e50189bd
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-09 03:02:12 +0100

    Update en_spiders.py

--- scrapyextractors/scrapyextractors/spiders/en_spiders.py
+++ scrapyextractors/scrapyextractors/spiders/en_spiders.py
@@ -19,7 +19,7 @@ conn = psycopg2.connect(user=POSTGRES_USER, password=POSTGRES_PASS,
 cursor = conn.cursor()
 conn.autocommit = True
 
-query = '''SELECT * from articles where lang like 'en' and name is null '''
+query = '''SELECT * from articles where lang like 'en' and name is null and description is null and devimages is null'''
 
 querlist = [query]
 for q in querlist:
