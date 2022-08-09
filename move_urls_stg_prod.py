f1f771e031569d965369c304be26949ed215b859
chdessid <108822042+chdessid@users.noreply.github.com>
2022-08-09 19:22:27 +0100

    Update move_urls_stg_prod.py

--- move_urls_stg_prod.py
+++ move_urls_stg_prod.py
@@ -2,7 +2,9 @@ import psycopg2
 import pandas as pd
 import pandas.io.sql as sqlio
 from decouple import config
+import math
 
+LIMIT_SELECT = 1000
 STG_POSTGRES_HOST = config('STG_POSTGRES_HOST')
 PRD_POSTGRES_HOST = config('PRD_POSTGRES_HOST')
 STG_DATABASE_NAME = config('STG_DATABASE_NAME')
@@ -22,18 +24,22 @@ prod_pgconn = psycopg2.connect(user=POSTGRES_USER,password=POSTGRES_PASS,
 prod_pgcursor = prod_pgconn.cursor()
 prod_pgconn.autocommit = True
 
-selectall = ''' select * from articles where name is not null and description is not null and devimages is not null and prod_ok is null '''
-stg_records = sqlio.read_sql_query(selectall, stg_pgconn)
+_countrecords = sqlio.read_sql_query(''' select count (*) from articles''', stg_pgconn)
+countrecords = (_countrecords["count"].values)[0]
+iteration = int(math.modf((countrecords/LIMIT_SELECT))[1])
 
-print ("TOTAL STG RECORDS TO BE MOVED TO PROD :::  {} ::: RECORDS".format(len(stg_records.index)),)
-for row, index in stg_records.iterrows():
-    print ("MOVING/ MARKING STG URLS TO PROD : {}".format(index["urls"]))
-    send_to_prod = ''' insert into articles (urls) values (%s) on conflict do nothing '''
-    query_prod_pgcursor = prod_pgcursor.execute(send_to_prod,[index["urls"]])
+for i in range(1,(iteration)) :
+    selectall = ''' select * from articles limit {}'''.format(LIMIT_SELECT)
+    stg_records = sqlio.read_sql_query(selectall, stg_pgconn)
     
-    addattributes = ''' update articles set name =%s, description =%s , devimages =%s where urls =%s '''
-    query_prod_pgcursor_attributes = prod_pgcursor.execute(addattributes,[index["name"].encode('ascii', 'ignore').decode('ascii'),index["description"].encode('ascii', 'ignore').decode('ascii'),
-                                                                        index["devimages"].encode('ascii', 'ignore').decode('ascii'),index["urls"]])
-    mark_prod_ok = ''' update articles set prod_ok=TRUE where urls =%s '''
-    query_mark_prod_ok = stg_pgcursor.execute(mark_prod_ok,[index["urls"]])
-    print ("PROD RECORDS MOVED : {}".format(index["urls"]))
+    for row, index in stg_records.iterrows():
+        print ("MOVING/ MARKING STG URLS TO PROD : {}".format(index["urls"]))
+        send_to_prod = ''' insert into articles (urls) values (%s) on conflict do nothing '''
+        query_prod_pgcursor = prod_pgcursor.execute(send_to_prod,[index["urls"]])
+        
+        addattributes = ''' update articles set name =%s, description =%s , devimages =%s where urls =%s '''
+        query_prod_pgcursor_attributes = prod_pgcursor.execute(addattributes,[index["name"].encode('ascii', 'ignore').decode('ascii'),index["description"].encode('ascii', 'ignore').decode('ascii'),
+                                                                            index["devimages"].encode('ascii', 'ignore').decode('ascii'),index["urls"]])
+        mark_prod_ok = ''' update articles set prod_ok=TRUE where urls =%s '''
+        query_mark_prod_ok = stg_pgcursor.execute(mark_prod_ok,[index["urls"]])
+        print ("PROD RECORDS MOVED : {}".format(index["urls"]))
